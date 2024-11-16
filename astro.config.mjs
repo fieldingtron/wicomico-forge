b2cf4092b6f09f8a011d1a095563ab1ab07b639e
Fields Marshall <fields.marshall@gmail.com>
2024-11-15 16:02:24 -0800

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from 'astro/config'
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  integrations: [tailwind()],
+  vite: {
+    css: {
+      preprocessorOptions: {
+        css: {
+          additionalData: '@import "./src/css/styles.css";',
+        },
+      },
+    },
+  },
+})
---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+module.exports = {
+  content: ['./src/**/*.{html,astro,js,jsx,ts,tsx}', './public/**/*.html'],
+  theme: {
+    extend: {
+      colors: {
+        redPinstripe: '#ff0000',
+      },
+    },
+  },
+  plugins: [],
+}
